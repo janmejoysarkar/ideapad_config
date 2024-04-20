f1e9c898e9567a22567828a654d02a12008cd604
janmejoy <janmejoysarker@gmail.com>
2024-04-20 16:06:47 +0530

    Files updated

--- i3_config_backup/backup.sh
+++ i3_config_backup/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Made to rsync config files from the .config folder to the scriptss folder.
-rsync -rntv --progress --delete $HOME/.bashrc $HOME/scripts/i3_config_backup/bashrc
-rsync -rntv --progress --delete $HOME/.config/{i3,i3blocks,alacritty} $HOME/scripts/i3_config_backup/
+rsync -rtv --progress --delete $HOME/.bashrc $HOME/scripts_git/i3_config_backup/bashrc
+rsync -rtv --progress --delete $HOME/.config/{i3,i3blocks,alacritty} $HOME/scripts_git/i3_config_backup/
