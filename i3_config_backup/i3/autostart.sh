7ddefa0fe32652c5aa8affe6c97e8d87ee260b0d
janmejoy <janmejoysarker@gmail.com>
2024-04-21 09:26:51 +0530

    Changes made to incorporate no screen blanking

--- i3_config_backup/i3/autostart.sh
+++ i3_config_backup/i3/autostart.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-$HOME/.config/i3/i3_monitor.sh
+#$HOME/.config/i3/i3_monitor.sh
 dunst &
 picom &
-./.dropbox-dist/dropboxd &
-xset s noblank & #disables screensaver screenblanking
-xset s off & #disables screensaver
-xset dpms 0 0 0 &
+xset s off -dpms& #disables screensaver and turns off dpms
 blueman-applet &
+nitrogen --restore
 notify-send 'Hello' $USER &
-
