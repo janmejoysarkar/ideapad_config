3e508d47656824b6beba262975b746b7cf6b8a76
janmejoy <janmejoysarker@gmail.com>
2024-05-03 12:50:03 +0530

    updated

--- i3_config_backup/i3/autostart.sh
+++ i3_config_backup/i3/autostart.sh
@@ -2,7 +2,10 @@
 #$HOME/.config/i3/i3_monitor.sh
 dunst &
 picom &
-xset s off -dpms& #disables screensaver and turns off dpms
+#xset s off -dpms & #disables screensaver and turns off dpms
+xset s 540 #screen saver enable in 9 mins
+xset dpms 600 600 600 #turn off screen in 10 mins
 blueman-applet &
 nitrogen --restore
+python3 /home/janmejoy/scripts_git/dropbox.py start &
 notify-send 'Hello' $USER &
